1bf4a8174c4fe548801156280742e1ca70ecebe5
frederho <frederikkholst@gmail.com>
2016-02-09 08:51:24 +0100

    Created card information form for page

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+var step = 1;
+window.onload = resize;
+
+function resize() {
+  var height = document.getElementsByTagName("html")[0].scrollHeight;
+  window.parent.postMessage(["setHeight", height], "*"); 
+}
+
+function goFullScreen () {
+    window.parent.postMessage(['goFullScreen', {}], "*");
+    location.href = "http://localhost:3003/test2.html"
+}
+
+function hideFrame() {
+    window.parent.postMessage(['hideFrame', {}], "*")
+}
+function next() {
+    showAndIncrement();
+    resize();
+}
+
+function displayCardForm () {
+    $('#creditCardForm').show();
+    resize();
+}
+
+function showAndIncrement() {
+    $('#section'+step).show();
+    step++;
+}
+
+function hideAndReduce() {
+    $('#section'+step).show();
+}
